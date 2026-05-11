d4176e3f66f38b436407e9b462dc73919bbf5131
Thisura Gallage <tggallage1@gmail.com>
2026-05-11 14:17:17 +0530

    drop billing sidebar comment

--- en/sidebars.ts
+++ en/sidebars.ts
@@ -2083,7 +2083,6 @@ const sidebars: SidebarsConfig = {
                 'manage/cloud/observability/anomaly-detection-alerts',
               ],
             },
-            // Billing
             {
               type: 'category',
               label: 'Billing',
