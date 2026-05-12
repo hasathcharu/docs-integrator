f27cf20379f126af5d961c73299c0695b219a756
Thisura Gallage <tggallage1@gmail.com>
2026-05-12 08:00:35 +0530

    add cloud usage dashboards page

--- en/sidebars.ts
+++ en/sidebars.ts
@@ -2110,6 +2110,13 @@ const sidebars: SidebarsConfig = {
                 'manage/cloud/platform-services/managed-databases',
               ],
             },
+            {
+              type: 'category',
+              label: 'Billing',
+              items: [
+                'manage/cloud/billing/usage-dashboards',
+              ],
+            },
           ],
         },
         {
